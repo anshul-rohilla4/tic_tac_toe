b54ab435e9100aeca476dec0ccbe1c9812382d58
anshul rohilla <113432929+anshul-rohilla4@users.noreply.github.com>
2024-02-05 22:57:39 +0530

    Update code.cpp

--- code.cpp
+++ code.cpp
@@ -6,78 +6,62 @@ using namespace std;
 char XO[3][3]={{' ',' ',' '},
               {' ',' ',' '},
               {' ',' ',' '}};
+
 int row,col;
 int c=0;//chance
 int x;//check_win
+int zz=0; //map
 void chance();
 void check_win();
 void _map();
 
 
+
+
+
+
+
+
+void check_win(){
+    if((XO[0][0]==XO[0][1] && XO[0][0]==XO[0][2]) //check_rows
+    || (XO[1][0]==XO[1][1] && XO[1][0]==XO[1][2])
+    || (XO[2][0]==XO[2][1] && XO[2][0]==XO[2][2])
+
+    || (XO[0][0]==XO[1][0] && XO[0][0]==XO[2][0]) //check_col
+    || (XO[0][1]==XO[1][1] && XO[0][1]==XO[2][1])
+    || (XO[0][2]==XO[1][2] && XO[0][2]==XO[2][2])
+
+    || (XO[0][0]==XO[1][1] && XO[0][0]==XO[2][2])//check_diagonal
+    || (XO[0][2]==XO[1][1] && XO[0][2]==XO[2][0]))
+
+    { x=1;}
+    else
+        {x=0;}
+}
+
+
 void chance(){
 
-    while(c <9 ){
-
-                if (x==1){
-                cout<<"finish"<<endl;
-
-    }
-    else if(x==0){
             if(c%2==0 ){
                 cout<<"p1 enter the row and col"<<endl;
                 cin>>row>>col;
                 XO[row][col]='X';
                 c++;
-                break;
-
-
 
             }
 
-          if(!c%2==0){
+          else if(!(c%2==0)){
               cout<<"p2 enter the row and col"<<endl;
               cin>>row>>col;
               XO[row][col]='O';
               c++;
-              break;
-
         }
-
-}
-}
-}
-
-
-void check_win(){
-    if(XO[0][0]==XO[0][1]==XO[0][2] //check_rows
-    || XO[1][0]==XO[1][1]==XO[1][2]
-    || XO[2][0]==XO[2][1]==XO[2][2]
-
-    || XO[0][0]==XO[1][0]==XO[2][0] //check_col
-    || XO[0][1]==XO[1][1]==XO[2][1]
-    || XO[0][2]==XO[1][2]==XO[2][2]
-
-    || XO[0][0]==XO[1][1]==XO[2][2] //check_diagonal
-    || XO[0][2]==XO[1][1]==XO[2][0])
-
-    { x=1;}
-
-    else{
-         x=0;
-    }
-
-
-
-
-
 }
 
 void _map(){
-    int zz=0;
-    while(zz <9){
 
-            check_win();
-             chance();
+    while(zz <9){
+        chance();
 
         printf("\t|\t|\n");
         cout<<"   "<<XO[0][0]<<"\t|  "<<XO[0][1]<<"    |  "<<XO[0][2]<<endl;
@@ -89,11 +73,11 @@ void _map(){
         cout<<"   "<<XO[2][0]<<"\t|  "<<XO[2][1]<<"    |  "<<XO[2][2]<<endl;
         printf("\t|\t|\n\n");
 
-
         zz++;
+        if (x==1){
+            break;
+        }
     }
-
-
 }
 
 
@@ -111,31 +95,17 @@ int main()
     cout<<"1. start "<<endl;
     cout<<"2. exit "<<endl;
 
-
-
-
-
     int play;
     cout<<"enter your choice"<<endl;
     cin>>play;
-    int zz;
+
     char p1='X';char p2='O';
     printf("\n\n");
     if(play==1){
             cout<<"\tthe game begins\n\n\n"<<endl;
-
             printf("player1 is playes as X \nplayer2 is playes as O\n\n\n");
             _map();
-
     }
-
-
-
-
-
-
-
-
     return 0;
 }
 
